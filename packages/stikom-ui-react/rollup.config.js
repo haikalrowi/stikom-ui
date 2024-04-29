db00961c9b2995ea3fe38b1d0b111c96f4005381
Haikal Rowi <haikalbintang27@gmail.com>
2024-04-29 12:27:42 +0700

    Update rollup.config.js

--- packages/stikom-ui-react/rollup.config.js
+++ packages/stikom-ui-react/rollup.config.js
@@ -1,21 +1,21 @@
 // @ts-check
-import terser from "@rollup/plugin-terser";
-import typescript from "@rollup/plugin-typescript";
+
 import { createRequire } from "node:module";
 import { defineConfig } from "rollup";
 
+import typescript from "@rollup/plugin-typescript";
+
 const require = createRequire(import.meta.url);
 const pkg = require("./package.json");
-const tsconfig = require("./tsconfig.json");
 
 export default defineConfig({
   external: [
     ...Object.keys(pkg.dependencies),
     ...Object.keys(pkg.peerDependencies),
   ],
-  input: tsconfig.files,
+  input: "./src/index.ts",
   output: {
-    file: pkg.main,
+    dir: "./dist",
   },
-  plugins: [typescript(), terser()],
+  plugins: [typescript()],
 });
